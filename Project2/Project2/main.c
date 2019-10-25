f19ce40f52c1125d06eb7435ba048d81644d2bbb
xxiaotianxin <56794751+xxiaotianxin@users.noreply.github.com>
2019-10-25 23:07:43 +0800

    11
    first file

---
+++ Project2/Project2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a = 1, b = 2;
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	printf("a=%d, b=%d\n", a, b);
+	system("pause");
+	return 0;
+}
